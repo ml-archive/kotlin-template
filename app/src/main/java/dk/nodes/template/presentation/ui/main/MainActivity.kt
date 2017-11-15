7d5bdf3cae9e7a0059373bcdffa650ab4285f73d
Moch <moch@nodes.dk>
2017-11-15 11:28:47 +0100

    - Implemented UI tests dependencies, samples etc.

--- app/src/main/java/dk/nodes/template/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/dk/nodes/template/presentation/ui/main/MainActivity.kt
@@ -1,13 +1,16 @@
 package dk.nodes.template.presentation.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import dk.nodes.nstack.kotlin.NStack
 import dk.nodes.nstack.kotlin.UpdateType
-import dk.nodes.template.domain.models.Post
 import dk.nodes.template.App
 import dk.nodes.template.R
+import dk.nodes.template.domain.models.Post
 import dk.nodes.template.domain.models.Translation
 import dk.nodes.template.presentation.injection.DaggerPresentationComponent
 import dk.nodes.template.presentation.injection.PresentationComponent
@@ -15,23 +18,24 @@ import dk.nodes.template.presentation.injection.PresentationModule
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity(), MainContract.View {
+ class MainActivity : AppCompatActivity(), MainContract.View {
+
     val component: PresentationComponent by lazy {
         DaggerPresentationComponent.builder()
                 .appComponent((application as App).appComponent)
                 .presentationModule(PresentationModule())
                 .build()
     }
-    @Inject lateinit var presenter : MainContract.Presenter
-
+    @Inject lateinit var presenter: MainContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         component.inject(this)
+        initUiTestSamples()
 
-        //textTv = findViewById(R.id.text) as TextView
-        textTv.text = Translation.defaultSection.settings
+        //textview = findViewById(R.id.textview) as TextView
+        textview.text = Translation.defaultSection.settings
         NStack.translate(this@MainActivity)
 
 
@@ -51,6 +55,26 @@ class MainActivity : AppCompatActivity(), MainContract.View {
     }
 
 
+     //This is for the SampleActivityTest.class for Ui tests examples.
+    private fun initUiTestSamples() {
+        saveButton.setOnClickListener(View.OnClickListener {
+            textview.text = edittext.text
+        })
+
+        buttonOpenDialog.setOnClickListener(View.OnClickListener {
+            AlertDialog.Builder(this)
+                    .setTitle("Hello Ui Test!")
+                    .setMessage("This is an alert message. Let the UI test read it")
+                    .setNegativeButton("Cancel", null)
+                    .setPositiveButton("OK", null)
+                    .show()
+        })
+
+        buttonOpenActivity.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this, SampleActivity::class.java))
+        })
+    }
+
     override fun onResume() {
         super.onResume()
         presenter.attachView(this)
@@ -63,8 +87,7 @@ class MainActivity : AppCompatActivity(), MainContract.View {
     }
 
     override fun showPosts(posts: List<Post>) {
-        for(post in posts)
-        {
+        for (post in posts) {
             Log.d("debug", post.toString())
         }
     }
