2993a1d08bdd9b7ffaff028b8f5afceacecd0c1f
Morten Pløger <mopl@nodes.dk>
2017-11-16 13:33:14 +0100

    made the pref manager less complicated since it is already thread safe

--- app/src/main/java/dk/nodes/template/storage/PrefManagerImpl.kt
+++ app/src/main/java/dk/nodes/template/storage/PrefManagerImpl.kt
@@ -9,24 +9,7 @@ import dk.nodes.template.domain.managers.PrefManager
  * Created by bison on 11/10/17.
  */
 class PrefManagerImpl(context: Context) : PrefManager {
-    var sharedPrefs : SharedPreferences = context.getSharedPreferences("${BuildConfig.APPLICATION_ID}.SHARED_PREFERENCES", Context.MODE_PRIVATE)
-    var editor : SharedPreferences.Editor? = null
-    override var isLocked : Boolean = false
-        get() = editor != null
-
-    override fun lock() {
-        if(isLocked)
-            return
-        editor = sharedPrefs.edit()
-    }
-
-    override fun unlock()
-    {
-        if(!isLocked)
-            return
-        editor?.commit()
-        editor = null
-    }
+    private var sharedPrefs : SharedPreferences = context.getSharedPreferences("${BuildConfig.APPLICATION_ID}.SHARED_PREFERENCES", Context.MODE_PRIVATE)
 
     override fun getInt(key : String, defaultValue : Int) : Int
     {
@@ -35,75 +18,43 @@ class PrefManagerImpl(context: Context) : PrefManager {
 
     override fun setInt(key : String, value : Int)
     {
-        if(!isLocked)
-            return
-        editor?.let {
-            it.putInt(key, value)
-        }
+        sharedPrefs.edit().putInt(key, value).commit()
     }
 
     override fun getLong(key : String, defaultValue : Long) : Long
-    {
-        return sharedPrefs.getLong(key, defaultValue)
-    }
+            = sharedPrefs.getLong(key, defaultValue)
 
     override fun setLong(key : String, value : Long)
     {
-        if(!isLocked)
-            return
-        editor?.let {
-            it.putLong(key, value)
-        }
+        sharedPrefs.edit().putLong(key, value).commit()
     }
 
     override fun getBoolean(key : String, defaultValue : Boolean) : Boolean
-    {
-        return sharedPrefs.getBoolean(key, defaultValue)
-    }
+            = sharedPrefs.getBoolean(key, defaultValue)
 
     override fun setBoolean(key : String, value : Boolean)
     {
-        if(!isLocked)
-            return
-        editor?.let {
-            it.putBoolean(key, value)
-        }
+        sharedPrefs.edit().putBoolean(key, value).commit()
     }
 
     override fun getFloat(key : String, defaultValue : Float) : Float
-    {
-        return sharedPrefs.getFloat(key, defaultValue)
-    }
+            = sharedPrefs.getFloat(key, defaultValue)
 
     override fun setFloat(key : String, value : Float)
     {
-        if(!isLocked)
-            return
-        editor?.let {
-            it.putFloat(key, value)
-        }
+        sharedPrefs.edit().putFloat(key, value).commit()
     }
 
     override fun getString(key : String, defaultValue : String?) : String?
-    {
-        return sharedPrefs.getString(key, defaultValue)
-    }
+            = sharedPrefs.getString(key, defaultValue)
 
     override fun setString(key : String, value : String)
     {
-        if(!isLocked)
-            return
-        editor?.let {
-            it.putString(key, value)
-        }
+        sharedPrefs.edit().putString(key, value).commit()
     }
 
     override fun remove(key : String)
     {
-        if(!isLocked)
-            return
-        editor?.let {
-            it.remove(key)
-        }
+        sharedPrefs.edit().remove(key).commit()
     }
 }
