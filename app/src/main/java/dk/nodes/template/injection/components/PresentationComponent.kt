eb98c7db3491e683aad544f5a86b0f93d4df42a7
Stinus Petersen <stpe@nodes.dk1>
2017-12-06 15:49:32 +0100

    updated to actually work and compile again, after brian fucked it up

--- app/src/main/java/dk/nodes/template/injection/components/PresentationComponent.kt
+++ app/src/main/java/dk/nodes/template/injection/components/PresentationComponent.kt
@@ -1,13 +1,11 @@
-package dk.nodes.template.presentation.injection
+package dk.nodes.template.injection.components
 
 import dagger.Component
 import dagger.Module
 import dagger.Provides
 import dk.nodes.arch.domain.injection.scopes.ActivityScope
-import dk.nodes.template.domain.interactors.GetPostsInteractor
-import dk.nodes.template.injection.components.AppComponent
+import dk.nodes.template.injection.modules.PresentationModule
 import dk.nodes.template.presentation.ui.main.MainActivity
-import dk.nodes.template.presentation.ui.main.MainContract
 import dk.nodes.template.presentation.ui.main.MainPresenter
 
 /**
@@ -20,12 +18,4 @@ interface PresentationComponent {
     fun inject(target : MainActivity)
     fun inject(target : MainPresenter)
 
-}
-
-@Module
-class PresentationModule {
-    @ActivityScope
-    @Provides fun provideMainPresenter(getPostsInteractor: GetPostsInteractor) : MainContract.Presenter {
-        return MainPresenter(getPostsInteractor)
-    }
 }
---
+++ app/src/main/java/dk/nodes/template/injection/modules/PresentationModule.kt
@@ -0,0 +1,20 @@
+package dk.nodes.template.injection.modules
+
+import dagger.Module
+import dagger.Provides
+import dk.nodes.arch.domain.injection.scopes.ActivityScope
+import dk.nodes.template.domain.interactors.GetPostsInteractor
+import dk.nodes.template.presentation.ui.main.MainContract
+import dk.nodes.template.presentation.ui.main.MainPresenter
+
+/**
+ * Created by bison on 06/12/17.
+ */
+@Module
+class PresentationModule {
+    @ActivityScope
+    @Provides
+    fun provideMainPresenter(getPostsInteractor: GetPostsInteractor) : MainContract.Presenter {
+        return MainPresenter(getPostsInteractor)
+    }
+}
