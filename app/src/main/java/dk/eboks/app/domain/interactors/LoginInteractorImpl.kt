c7b464e54c8ba91ba63299245768fb3bc9d47137
Stinus Petersen <stpe@nodes.dk1>
2017-12-19 15:54:50 +0100

    changed branch name and minor updates

--- app/src/main/java/dk/eboks/app/domain/interactors/LoginInteractorImpl.kt
+++ app/src/main/java/dk/eboks/app/domain/interactors/LoginInteractorImpl.kt
@@ -5,11 +5,9 @@ import dk.eboks.app.BuildConfig
 import dk.eboks.app.domain.managers.ProtocolManager
 import dk.eboks.app.domain.models.request.AppInfo
 import dk.eboks.app.domain.models.request.LoginRequest
-import dk.eboks.app.domain.models.request.UserInfo
 import dk.eboks.app.network.Api
 import dk.nodes.arch.domain.executor.Executor
 import dk.nodes.arch.domain.interactor.BaseInteractor
-import timber.log.Timber
 
 /**
  * Created by bison on 24-06-2017.
@@ -20,7 +18,6 @@ class LoginInteractorImpl(executor: Executor, val api: Api, val protocolManager:
 
     override fun execute() {
         // we don't use input in this example but we could:
-
         try {
             input?.let {
                 // do something with unwrapped input
