e9537fa6fac8acae43443865feeb29e84265cc9e
Brian Estrada <lovesguitar@gmail.com>
2018-03-16 10:54:49 +0100

    Added CI Integration

--- app/src/main/java/dk/eboks/app/domain/models/local/Settings.kt
+++ app/src/main/java/dk/eboks/app/domain/models/local/Settings.kt
@@ -2,9 +2,6 @@ package dk.eboks.app.domain.models.local
 
 import java.io.Serializable
 
-/**
- * Created by bison on 24-06-2017.
- */
 data class Settings (
     var deviceId : String
 ) : Serializable
--- app/src/main/java/dk/eboks/app/domain/models/protocol/ErrorType.kt
+++ app/src/main/java/dk/eboks/app/domain/models/protocol/ErrorType.kt
@@ -2,14 +2,10 @@ package dk.eboks.app.domain.models.protocol
 
 import com.google.gson.annotations.SerializedName
 
-/**
- * Created by bison on 09-02-2018.
- */
 enum class ErrorType(val type : String) {
     @SerializedName("error") ERROR("error"),
     @SerializedName("warning") WARNING("warning"),
     @SerializedName("information") INFORMATION("information");
-
     override fun toString(): String {
         when(this)
         {
