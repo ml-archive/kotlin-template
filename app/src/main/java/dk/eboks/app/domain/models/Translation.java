176473ab4b6da3b2ef7b38a937eb0ed1688fafd4
Stinus Petersen <stpe@nodes.dk1>
2018-03-26 09:24:27 +0200

    translation updates

--- app/src/main/java/dk/eboks/app/domain/models/Translation.java
+++ app/src/main/java/dk/eboks/app/domain/models/Translation.java
@@ -3,7 +3,7 @@ package dk.eboks.app.domain.models;
 /**
  * Created by nstack.io gradle translation plugin
  * Built from Accept Header: en-US 
- * Generated: Fri Mar 23 15:38:42 CET 2018 
+ * Generated: Mon Mar 26 09:20:43 CEST 2018 
  */
 
 public class Translation {
