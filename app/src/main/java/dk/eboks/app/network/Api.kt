0bad6dfe72e41e0e1126c4d4497663799c53dcda
Christian Nikolaj Thomsen <chnt@nodes.dk>
2018-05-08 09:35:38 +0200

    aligned all API calls to use the same url scheme

--- app/src/main/java/dk/eboks/app/network/Api.kt
+++ app/src/main/java/dk/eboks/app/network/Api.kt
@@ -33,11 +33,10 @@ interface Api {
     // @GET("regions") fun getRegions() : Call<List<Region>>
     @GET("mail/categories") fun getMailCategories() : Call<List<Folder>>
     @GET("mail/folders") fun getFolders() : Call<List<Folder>>
-    @GET("folders/{id}/messages") fun getMessages(@Path("id") id : Long) : Call<List<Message>>
+    @GET("mail/folders/{id}/messages") fun getMessages(@Path("id") id : Long) : Call<List<Message>>
     @GET("mail/senders/{id}/messages") fun getMessagesBySender(@Path("id") id : Long) : Call<List<Message>>
     @GET("mail/folders/{folderId}/messages/{id}") fun getMessage(@Path("id") id : String, @Path("folderId") folderId : Long, @Query("receipt") receipt : Boolean? = null, @Query("terms") terms : Boolean? = null) : Call<Message>
     @GET("mail/{type}/messages") fun getMessagesByType(@Path("type") type : String) : Call<List<Message>>
-    @GET("senders") fun getSenders() : Call<List<Sender>>
     @PUT("session") fun login(@Body body : LoginRequest) : Single<BufferedSource>
 
     // reply forms
@@ -59,6 +58,7 @@ interface Api {
     @GET("groups/{segment}/categories") fun getSenderCategories(@Path("segment") segment: String ) : Call<List<SenderCategory>> // private or public
     @GET("groups/categories/{id}/senders") fun getSenders(@Path("id") categoryId : Long) : Call<SenderCategory>   // TODO: shouldn't this be called "/api/groups/private/categories/{id}" ??
     @GET("groups/senders") fun searchSenders(@Query("searchText") searchText : String) : Call<List<Sender>>
+    @GET("senders") fun getSenders() : Call<List<Sender>>
     @GET("groups/segments/{id}") fun getSegmentDetail(@Path("id") segmentId : Long) : Call<Segment>               // segment detail
     @GET("groups/senders/{id}") fun getSenderDetail(@Path("id") senderId : Long) : Call<Sender>
 
