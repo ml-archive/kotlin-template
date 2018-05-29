6a561d247f8741f0881bcb77ade317cd85954329
Vlad <vovasoft@yahoo.it>
2018-05-29 17:47:58 +0100

    Updated Nstack dependency. Updated Nstack plugin. Replaced old Nstack usage with new one.

--- app/src/main/java/dk/nodes/template/presentation/base/BaseActivity.kt
+++ app/src/main/java/dk/nodes/template/presentation/base/BaseActivity.kt
@@ -1,15 +1,22 @@
 package dk.nodes.template.presentation.base
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import dk.nodes.arch.presentation.base.BaseView
+import dk.nodes.nstack.kotlin.inflater.NStackBaseContext
 
 abstract class BaseActivity : AppCompatActivity(), BaseView {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //Make sure all of our dependencies get injected
         injectDependencies()
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(NStackBaseContext(newBase))
     }
 
     override fun onStart() {
