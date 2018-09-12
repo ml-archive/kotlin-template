96cc140130283f56b36208b249a113d3c20609a6
lucassales <saless182@gmail.com>
2018-09-12 15:08:20 +0200

    Applying spotless

--- app/src/main/java/dk/nodes/template/inititializers/TimberInitializer.kt
+++ app/src/main/java/dk/nodes/template/inititializers/TimberInitializer.kt
@@ -5,7 +5,7 @@ import dk.nodes.template.BuildConfig
 import timber.log.Timber
 import javax.inject.Inject
 
-class TimberInitializer @Inject constructor(): AppInitializer {
+class TimberInitializer @Inject constructor() : AppInitializer {
     override fun init(app: Application) {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
