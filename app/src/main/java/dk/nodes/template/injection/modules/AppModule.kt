57b654799e48c4031d08e0d164bcb33009213269
lucassales <saless182@gmail.com>
2018-12-16 16:41:17 -0200

    Move Dispatchers and Schedulers to ExecutorModeule

--- app/src/main/java/dk/nodes/template/injection/modules/AppModule.kt
+++ app/src/main/java/dk/nodes/template/injection/modules/AppModule.kt
@@ -3,16 +3,10 @@ package dk.nodes.template.injection.modules
 import android.content.Context
 import dagger.Module
 import dagger.Provides
-import dk.nodes.arch.domain.injection.scopes.AppScope
-import dk.nodes.arch.util.AppCoroutineDispatchers
-import dk.nodes.arch.util.AppRxSchedulers
 import dk.nodes.template.App
 import dk.nodes.template.inititializers.AppInitializers
 import dk.nodes.template.inititializers.NStackInitializer
 import dk.nodes.template.inititializers.TimberInitializer
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import kotlinx.coroutines.Dispatchers
 
 @Module
 class AppModule {
@@ -26,20 +20,4 @@ class AppModule {
     ): AppInitializers {
         return AppInitializers(nStackInitializer, timberInitializer)
     }
-
-    @AppScope
-    @Provides
-    fun provideRxSchedulers(): AppRxSchedulers = AppRxSchedulers(
-        io = Schedulers.io(),
-        computation = Schedulers.computation(),
-        main = AndroidSchedulers.mainThread()
-    )
-
-    @AppScope
-    @Provides
-    fun provideCoroutineDispatchers() = AppCoroutineDispatchers(
-        io = Dispatchers.IO,
-        computation = Dispatchers.Default,
-        main = Dispatchers.Main
-    )
 }
