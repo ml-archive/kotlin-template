79b5225c208673abd57b49980bc2153e16c5f971
lucassales <saless182@gmail.com>
2018-12-19 13:35:14 -0200

    Parcelable fix

--- app/src/main/java/dk/eboks/app/domain/models/channel/RequirementType.kt
+++ app/src/main/java/dk/eboks/app/domain/models/channel/RequirementType.kt
@@ -1,11 +1,14 @@
 package dk.eboks.app.domain.models.channel
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
 /**
  * Created by thsk on 19/02/2018.
  */
-enum class RequirementType(val type : String) {
+@Parcelize
+enum class RequirementType(val type : String): Parcelable {
     @SerializedName("identity") IDENTITY("identitiy"),
     @SerializedName("name") NAME("name"),
     @SerializedName("email") EMAIL("email"),
