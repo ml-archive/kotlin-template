6067764e3a769aa8e2c8c4cb68f7dc1109e3f69f
Roman Levinzon <levinzon.roman@gmail.com>
2019-02-14 15:35:58 +0100

    Added payment options recycler view

--- app/src/main/java/dk/eboks/app/presentation/ui/message/components/detail/payment/PaymentComponentFragment.kt
+++ app/src/main/java/dk/eboks/app/presentation/ui/message/components/detail/payment/PaymentComponentFragment.kt
@@ -1,14 +1,22 @@
 package dk.eboks.app.presentation.ui.message.components.detail.payment
 
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import dk.eboks.app.R
 import dk.eboks.app.domain.models.Translation
+import dk.eboks.app.domain.models.message.PaymentOption
+import dk.eboks.app.domain.models.shared.Description
+import dk.eboks.app.domain.models.shared.Link
 import dk.eboks.app.presentation.base.BaseFragment
+import dk.eboks.app.presentation.widgets.DividerItemDecoration
 import kotlinx.android.synthetic.main.fragment_payment_component.*
 
 
@@ -23,22 +31,23 @@ class PaymentComponentFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        paymentCreditCardTv.text = Translation.paymentdrawer.payWithPrefix
-        paymentServiceTv.text = Translation.paymentdrawer.registerAndPayWithPrefix
         paymentNotificationSwitch.text = Translation.paymentdrawer.notificationsHeader
         setupListeners()
-
+        setupRecyclerView()
     }
 
+    private fun setupRecyclerView() {
+        paymentOptionsRv.layoutManager = LinearLayoutManager(context)
+        paymentOptionsRv.adapter = PaymentOptionsAdapter(listOf(PaymentOption("card", description = Description("", "", link = Link()), status = 1, type = ""), PaymentOption("card", description = Description("", "", link = Link()), status = 1, type = "")))
+        paymentOptionsRv.addItemDecoration( DividerItemDecoration(
+                drawable = ContextCompat.getDrawable(context!!, R.drawable.shape_divider)!!,
+                indentationDp = 72,
+                backgroundColor = ContextCompat.getColor(context!!, R.color.white)
+        ))
+    }
 
     private fun setupListeners() {
-        paymentCardCL.setOnClickListener {
 
-        }
-
-        paymentServiceLL.setOnClickListener {
-
-        }
     }
 
 }
---
+++ app/src/main/java/dk/eboks/app/presentation/ui/message/components/detail/payment/PaymentOptionsAdapter.kt
@@ -0,0 +1,33 @@
+package dk.eboks.app.presentation.ui.message.components.detail.payment
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import dk.eboks.app.R
+import dk.eboks.app.domain.models.Translation
+import dk.eboks.app.domain.models.message.PaymentOption
+import kotlinx.android.synthetic.main.viewholder_payment_option.view.*
+
+class PaymentOptionsAdapter(val options: List<PaymentOption>) : RecyclerView.Adapter<PaymentOptionsAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.viewholder_payment_option, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = options.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindView(options[position])
+    }
+
+    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bindView(paymentOption: PaymentOption) {
+            view.paymentOptionIv.setImageResource(R.drawable.icon_48_alternative_bs_white)
+            view.paymentOptionTv.text = "${Translation.paymentdrawer.payWithPrefix} ${paymentOption.name}"
+        }
+    }
+
+}
