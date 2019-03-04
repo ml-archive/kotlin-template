e2392322b369fac559e91fad777b452882001b46
Roman Levinzon <levinzon.roman@gmail.com>
2019-03-04 11:43:17 +0100

    Adjusted GetFoldersInteractor, added GetFoldersInteractorTest

--- mail-interactors/src/main/java/dk/eboks/app/mail/domain/interactors/folder/GetFoldersInteractorImpl.kt
+++ mail-interactors/src/main/java/dk/eboks/app/mail/domain/interactors/folder/GetFoldersInteractorImpl.kt
@@ -15,16 +15,26 @@ import javax.inject.Inject
  * Created by bison on 01/02/18.
  */
 internal class GetFoldersInteractorImpl @Inject constructor(
-    executor: Executor,
-    private val foldersRepository: FoldersRepository
+        executor: Executor,
+        private val foldersRepository: FoldersRepository
 ) : BaseInteractor(executor), GetFoldersInteractor {
     override var output: GetFoldersInteractor.Output? = null
     override var input: GetFoldersInteractor.Input? = null
 
     override fun execute() {
         val modeType = input?.pickermode ?: FolderMode.NORMAL
+
+        val folders = try {
+            foldersRepository.getFolders(input?.cached ?: true, input?.userId)
+        } catch (t: Throwable) {
+            runOnUIThread {
+                output?.onGetFoldersError(exceptionToViewError(t))
+            }
+            return
+        }
+        Timber.e("Got folders $folders")
+
         if (modeType == FolderMode.SELECT) {
-            val folders = foldersRepository.getFolders(input?.cached ?: true, input?.userId)
             runOnUIThread {
                 val pickerfolders = ArrayList<Folder>()
                 folders.forEach { f ->
@@ -39,26 +49,18 @@ internal class GetFoldersInteractorImpl @Inject constructor(
                 output?.onGetFolders(pickerfolders)
             }
         } else {
-            try {
-                val folders = foldersRepository.getFolders(input?.cached ?: true, input?.userId)
-                Timber.e("Got folders $folders")
-                val system = ArrayList<Folder>()
-                val user = ArrayList<Folder>()
-                folders.forEach { f ->
-                    if (f.type?.isSystemFolder() == true) {
-                        system.add(f)
-                    }
-                    if (f.type == FolderType.FOLDER)
-                        user.add(f)
+            val system = ArrayList<Folder>()
+            val user = ArrayList<Folder>()
+            folders.forEach { f ->
+                if (f.type?.isSystemFolder() == true) {
+                    system.add(f)
                 }
-                runOnUIThread {
-                    output?.onGetSystemFolders(system)
-                    output?.onGetFolders(user)
-                }
-            } catch (t: Throwable) {
-                runOnUIThread {
-                    output?.onGetFoldersError(exceptionToViewError(t))
-                }
+                if (f.type == FolderType.FOLDER)
+                    user.add(f)
+            }
+            runOnUIThread {
+                output?.onGetSystemFolders(system)
+                output?.onGetFolders(user)
             }
         }
     }
