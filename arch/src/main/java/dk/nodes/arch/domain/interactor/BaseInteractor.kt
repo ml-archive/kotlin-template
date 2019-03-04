9deb6b2cf6c7add551afc3ff6fad3df4c0c67c73
lucassales <saless182@gmail.com>
2019-03-04 09:37:06 +0100

    Removing submitToHockey on  BaseInteractor (moved to HockeyTree)

--- arch/src/main/java/dk/nodes/arch/domain/interactor/BaseInteractor.kt
+++ arch/src/main/java/dk/nodes/arch/domain/interactor/BaseInteractor.kt
@@ -12,6 +12,7 @@ abstract class BaseInteractor(protected val executor: Executor) : Interactor {
             } catch (t: Throwable) {
                 Timber.e("Uncaught throwable in thread ${Thread.currentThread().name}")
                 Timber.e(t)
+                throw t
             }
         })
     }
@@ -21,25 +22,4 @@ abstract class BaseInteractor(protected val executor: Executor) : Interactor {
     }
 
     abstract fun execute()
-
-    private fun submitToHockey(t: Throwable) {
-        try {
-            val exceptionHandlerCls = Class.forName("net.hockeyapp.android.ExceptionHandler")
-            try {
-                val defaultHandler =
-                    exceptionHandlerCls.cast(Thread.getDefaultUncaughtExceptionHandler())
-                val method = exceptionHandlerCls.getMethod(
-                    "uncaughtException",
-                    Thread::class.java,
-                    Throwable::class.java
-                )
-                method.invoke(defaultHandler, Thread.currentThread(), t)
-            } catch (ex: ClassCastException) {
-                // e.printStackTrace()
-                Timber.e("Could not get HockeySDK uncaught exception handler")
-            }
-        } catch (e: ClassNotFoundException) {
-            Timber.e("Could not load HockeyApp SDK Classes, cannot record crash")
-        }
-    }
 }
