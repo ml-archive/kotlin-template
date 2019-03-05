aaa5527c5d738384363a9ae1c7408e17d1149646
Roman Levinzon <levinzon.roman@gmail.com>
2019-03-05 14:10:05 +0100

    FolderShortcustPresenter unit-test

---
+++ mail-presentation/src/test/java/dk/eboks/app/mail/FolderShortcutsComponentPresenterTest.kt
@@ -0,0 +1,66 @@
+package dk.eboks.app.mail
+
+import androidx.lifecycle.Lifecycle
+import dk.eboks.app.domain.models.folder.Folder
+import dk.eboks.app.domain.models.local.ViewError
+import dk.eboks.app.mail.domain.interactors.categories.GetCategoriesInteractor
+import dk.eboks.app.mail.domain.interactors.folder.GetFoldersInteractor
+import dk.eboks.app.mail.domain.interactors.folder.OpenFolderInteractor
+import dk.eboks.app.mail.presentation.ui.components.foldershortcuts.FolderShortcutsComponentContract
+import dk.eboks.app.mail.presentation.ui.components.foldershortcuts.FolderShortcutsComponentPresenter
+import io.mockk.*
+import org.greenrobot.eventbus.EventBus
+import org.junit.Before
+import org.junit.Test
+
+class FolderShortcutsComponentPresenterTest {
+
+    private val getCategoriesInteractor = mockk<GetCategoriesInteractor>(relaxUnitFun = true)
+    private val openFolderInteractor = mockk<OpenFolderInteractor>(relaxUnitFun = true)
+    private val lifecycle = mockk<Lifecycle>(relaxUnitFun = true)
+    private val view = mockk<FolderShortcutsComponentContract.View>(relaxUnitFun = true)
+
+    private lateinit var presenter: FolderShortcutsComponentPresenter
+
+
+    @Before
+    fun setUp() {
+        presenter = FolderShortcutsComponentPresenter(getCategoriesInteractor, openFolderInteractor)
+        presenter.onViewCreated(view, lifecycle)
+    }
+
+
+    @Test
+    fun `Test Open Folder`() {
+        val folder = mockk<Folder>()
+        presenter.openFolder(folder)
+        verify {
+            openFolderInteractor.input = OpenFolderInteractor.Input(folder)
+            openFolderInteractor.run()
+        }
+    }
+
+    @Test
+    fun `Test On Get Categories`() {
+        val folders = mockk<List<Folder>>(relaxed = true)
+        presenter.onGetCategories(folders)
+        verify {
+            view.showProgress(false)
+            view.showFolders(folders)
+        }
+    }
+
+    @Test
+    fun `Test On Get Categories Error`() {
+        val error = ViewError()
+        presenter.onGetCategoriesError(error)
+        verify {
+            view.showErrorDialog(error)
+            view.showProgress(false)
+        }
+    }
+
+
+
+
+}
