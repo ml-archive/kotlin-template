511ffea6c86952dc853168089fb31dc562ca284d
lucassales <saless182@gmail.com>
2019-03-07 12:42:10 +0100

    Scope and disposables as extension vals

--- app/src/main/java/dk/nodes/template/presentation/base/BaseViewModel.kt
+++ app/src/main/java/dk/nodes/template/presentation/base/BaseViewModel.kt
@@ -2,19 +2,29 @@ package dk.nodes.template.presentation.base
 
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import java.io.Closeable
+import java.util.concurrent.ConcurrentHashMap
 
 open class BaseViewModel : ViewModel() {
-    private val job = Job()
+
+    private val tagMap = ConcurrentHashMap<String, Any>()
+
+    internal fun <T : Any> setTag(key: String, t: T): T {
+        return getTag<T>(key)
+            ?: {
+                tagMap[key] = t
+                t
+            }.invoke()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    internal fun <T> getTag(key: String) = tagMap[key] as? T
 
     protected val disposables = CompositeDisposable()
-    protected open val scope = CoroutineScope(Dispatchers.Main + job)
-
     override fun onCleared() {
         super.onCleared()
-        disposables.clear()
-        job.cancel()
+        tagMap.forEach { entry ->
+            (entry.value as? Closeable)?.close()
+        }
     }
 }
--- app/src/main/java/dk/nodes/template/presentation/ui/main/MainActivityViewModel.kt
+++ app/src/main/java/dk/nodes/template/presentation/ui/main/MainActivityViewModel.kt
@@ -3,10 +3,10 @@ package dk.nodes.template.presentation.ui.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import dk.nodes.template.domain.interactors.PostsInteractor
-import dk.nodes.template.domain.models.Post
 import dk.nodes.template.domain.models.Result
 import dk.nodes.template.domain.models.Translation
 import dk.nodes.template.presentation.base.BaseViewModel
+import dk.nodes.template.presentation.base.scope
 import dk.nodes.template.util.Event
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -14,7 +14,7 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class MainActivityViewModel @Inject constructor(
-        private val postsInteractor: PostsInteractor
+    private val postsInteractor: PostsInteractor
 ) : BaseViewModel() {
 
     private val _viewState = MutableLiveData<MainActivityViewState>()
@@ -25,17 +25,13 @@ class MainActivityViewModel @Inject constructor(
         val result = withContext(Dispatchers.IO) { postsInteractor.run() }
         when (result) {
             is Result.Success -> _viewState.value = _viewState.value?.copy(
-                    isLoading = false,
-                    posts = result.data
+                isLoading = false,
+                posts = result.data
             )
             is Result.Error -> _viewState.value = _viewState.value?.copy(
-                    isLoading = false,
-                    errorMessage = Event(Translation.error.unknownError)
+                isLoading = false,
+                errorMessage = Event(Translation.error.unknownError)
             )
         }
-    }
-
-    override fun onCleared() {
-        super.onCleared()
     }
 }
