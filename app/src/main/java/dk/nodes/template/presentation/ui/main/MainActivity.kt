fc06c4a396ed8afa60f46b204c68e162c53e3083
lucassales <saless182@gmail.com>
2019-03-27 11:35:00 +0100

    Teaking MainActivity LiveData observers

--- app/src/main/java/dk/nodes/template/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/dk/nodes/template/presentation/ui/main/MainActivity.kt
@@ -1,27 +1,15 @@
 package dk.nodes.template.presentation.ui.main
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AlertDialog
-import androidx.lifecycle.Observer
+import androidx.core.view.isVisible
 import com.google.android.material.snackbar.Snackbar
-import dk.nodes.nstack.kotlin.NStack
-import dk.nodes.nstack.kotlin.models.AppUpdate
-import dk.nodes.nstack.kotlin.models.AppUpdateState
-import dk.nodes.template.BuildConfig
 import dk.nodes.template.R
-import dk.nodes.template.domain.models.Post
 import dk.nodes.template.domain.models.Translation
 import dk.nodes.template.presentation.base.BaseActivity
-import dk.nodes.template.util.observeEvent
 import dk.nodes.template.util.observeNonNull
 import kotlinx.android.synthetic.main.activity_main.*
-import net.hockeyapp.android.CrashManager
-import net.hockeyapp.android.CrashManagerListener
 import net.hockeyapp.android.UpdateManager
-import timber.log.Timber
 
 class MainActivity : BaseActivity() {
 
@@ -33,9 +21,11 @@ class MainActivity : BaseActivity() {
         //setupNstack()
         //setupHockey()
         viewModel = bindViewModel()
-        viewModel.viewState.observe(this, Observer(this::showLoading))
-        viewModel.viewState.observe(this, Observer(this::showPosts))
-        viewModel.viewState.observe(this, Observer(this::showErrorMessage))
+        viewModel.viewState.observeNonNull(this) {
+            showLoading(it)
+            showPosts(it)
+            showErrorMessage(it)
+        }
         viewModel.fetchPosts()
     }
 
@@ -50,14 +40,18 @@ class MainActivity : BaseActivity() {
     }
 
     private fun showLoading(state: MainActivityViewState) {
-        postsProgressBar.visibility = if(state.isLoading) View.VISIBLE else View.GONE
+        postsProgressBar.isVisible = state.isLoading
     }
 
     private fun showErrorMessage(state: MainActivityViewState) {
         state.errorMessage?.let {
-            if(it.consumed) return@let
+            if (it.consumed) return@let
 
-            Snackbar.make(postsTextView, it.consume() ?: Translation.error.unknownError, Snackbar.LENGTH_SHORT)
+            Snackbar.make(
+                postsTextView,
+                it.consume() ?: Translation.error.unknownError,
+                Snackbar.LENGTH_SHORT
+            )
         }
     }
 }
