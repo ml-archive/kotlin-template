0f4826122df1981d54c1ea75112951eed6157b09
lucassales <saless182@gmail.com>
2019-04-09 15:57:56 +0200

    Update Extensions.kt

--- domain/src/main/java/dk/nodes/template/domain/extensions/Extensions.kt
+++ domain/src/main/java/dk/nodes/template/domain/extensions/Extensions.kt
@@ -1,4 +1,4 @@
-package dk.nodes.template.presentation.extensions
+package dk.nodes.template.domain.extensions
 
 inline fun <T> T.guard(block: T.() -> Unit): T {
     if (this == null) block(); return this
