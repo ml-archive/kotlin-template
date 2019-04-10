ae6d08abad3f92428ce2c88d520ea12359416dbd
lucassales <saless182@gmail.com>
2019-04-10 11:41:35 +0200

    Joinining AppBiningModule and AppModule

--- app/src/main/java/dk/nodes/template/injection/modules/AppModule.kt
+++ app/src/main/java/dk/nodes/template/injection/modules/AppModule.kt
@@ -1,16 +1,23 @@
 package dk.nodes.template.injection.modules
 
 import android.content.Context
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dk.nodes.template.App
+import dk.nodes.template.inititializers.AppInitializer
+import dk.nodes.template.inititializers.AppInitializerImpl
 
-@Module(
-    includes = [
-        AppBindingModule::class
-    ]
-)
-class AppModule {
-    @Provides
-    fun provideContext(application: App): Context = application.applicationContext
+@Module
+abstract class AppModule {
+
+    @Binds
+    abstract fun bindAppInitalizer(initializer: AppInitializerImpl): AppInitializer
+
+    @Module
+    companion object {
+        @JvmStatic
+        @Provides
+        fun provideContext(application: App): Context = application.applicationContext
+    }
 }
