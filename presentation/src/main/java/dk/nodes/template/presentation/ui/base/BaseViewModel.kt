f6660ab584c82da47ab46cf4adafbfbd3bd6a802
lucassales <saless182@gmail.com>
2019-04-11 18:02:32 +0200

    Generic ViewState on BaseViewModel

--- presentation/src/main/java/dk/nodes/template/presentation/ui/base/BaseViewModel.kt
+++ presentation/src/main/java/dk/nodes/template/presentation/ui/base/BaseViewModel.kt
@@ -1,13 +1,29 @@
 package dk.nodes.template.presentation.ui.base
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 
-open class BaseViewModel : ViewModel() {
+abstract class BaseViewModel<VS : Any> : ViewModel() {
     private val job = Job()
+    private val _viewState = MutableLiveData<VS>()
+    val viewState: LiveData<VS> = _viewState
+
+    protected var state: VS
+        get() {
+            return _viewState.value ?: {
+                val initViewState = initViewState()
+                _viewState.value = initViewState
+                initViewState
+            }.invoke()
+        }
+        set(value) = _viewState.postValue(value)
+
+    protected abstract fun initViewState(): VS
 
     protected val disposables = CompositeDisposable()
     protected open val scope = CoroutineScope(Dispatchers.Main + job)
--- presentation/src/main/java/dk/nodes/template/presentation/ui/main/MainActivityViewModel.kt
+++ presentation/src/main/java/dk/nodes/template/presentation/ui/main/MainActivityViewModel.kt
@@ -1,11 +1,9 @@
 package dk.nodes.template.presentation.ui.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import dk.nodes.template.domain.interactors.PostsInteractor
 import dk.nodes.template.domain.interactors.IResult
-import dk.nodes.template.presentation.ui.base.BaseViewModel
+import dk.nodes.template.domain.interactors.PostsInteractor
 import dk.nodes.template.presentation.nstack.Translation
+import dk.nodes.template.presentation.ui.base.BaseViewModel
 import dk.nodes.template.presentation.util.LiveEvent
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -14,23 +12,25 @@ import javax.inject.Inject
 
 class MainActivityViewModel @Inject constructor(
     private val postsInteractor: PostsInteractor
-) : BaseViewModel() {
+) : BaseViewModel<MainActivityViewState>() {
+    override fun initViewState(): MainActivityViewState {
+        return MainActivityViewState()
+    }
 
-    private val _viewState = MutableLiveData<MainActivityViewState>()
-    val viewState: LiveData<MainActivityViewState> = _viewState
-
-    fun fetchPosts() = scope.launch {
-        _viewState.value = MainActivityViewState(isLoading = true)
-        val result = withContext(Dispatchers.IO) { postsInteractor() }
-        when (result) {
-            is IResult.Success -> _viewState.value = _viewState.value?.copy(
-                isLoading = false,
-                posts = result.data
-            )
-            is IResult.Error -> _viewState.value = _viewState.value?.copy(
-                isLoading = false,
-                errorMessage = LiveEvent(Translation.error.unknownError)
-            )
+    fun fetchPosts() {
+        state = state.copy(isLoading = true)
+        scope.launch {
+            val result = withContext(Dispatchers.IO) { postsInteractor() }
+            state = when (result) {
+                is IResult.Success -> state.copy(
+                    isLoading = false,
+                    posts = result.data
+                )
+                is IResult.Error -> state.copy(
+                    isLoading = false,
+                    errorMessage = LiveEvent(Translation.error.unknownError)
+                )
+            }
         }
     }
 }
