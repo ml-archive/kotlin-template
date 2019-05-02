90e2d9fc1000ccc7b393b804804224552f329716
lucassales <saless182@gmail.com>
2019-05-02 12:03:43 +0200

    Tweaking result mapper

--- presentation/src/main/java/dk/nodes/template/presentation/ui/main/MainActivityViewModel.kt
+++ presentation/src/main/java/dk/nodes/template/presentation/ui/main/MainActivityViewModel.kt
@@ -4,10 +4,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Transformations
 import dk.nodes.template.domain.interactors.Fail
+import dk.nodes.template.domain.interactors.InteractorResult
 import dk.nodes.template.domain.interactors.Loading
 import dk.nodes.template.domain.interactors.PostsInteractor
 import dk.nodes.template.domain.interactors.Success
 import dk.nodes.template.domain.interactors.Uninitialized
+import dk.nodes.template.models.Post
 import dk.nodes.template.presentation.extensions.asChannel
 import dk.nodes.template.presentation.extensions.asLiveData
 import dk.nodes.template.presentation.extensions.asResult
@@ -21,6 +23,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import timber.log.Timber
 import javax.inject.Inject
 
 class MainActivityViewModel @Inject constructor(
@@ -35,71 +38,51 @@ class MainActivityViewModel @Inject constructor(
     val viewState: LiveData<MainActivityViewState> = _viewState
 
     init {
-        _viewState.addSource(Transformations.map(this.liveDataInteractor.liveData) {
-            when (it) {
-                is Success -> _viewState.value?.copy(posts = it.data, isLoading = false)
-                is Loading -> _viewState.value?.copy(isLoading = true)
-                is Fail -> _viewState.value?.copy(
-                    errorMessage = SingleEvent(Translation.error.unknownError), isLoading = false
-                )
-                is Uninitialized -> MainActivityViewState()
-            }
-        }, _viewState::postValue)
+        _viewState.addSource(
+            Transformations.map(this.liveDataInteractor.liveData, this::mapResult),
+            _viewState::postValue
+        )
 
         scope.launch {
             channelInteractor.receiveChannel.consumeEach {
-                _viewState.postValue(
-                    when (it) {
-                        is Success -> _viewState.value?.copy(posts = it.data, isLoading = false)
-                        is Loading -> viewState.value?.copy(isLoading = true)
-                        is Fail -> viewState.value?.copy(
-                            errorMessage = SingleEvent(Translation.error.unknownError),
-                            isLoading = false
-                        )
-                        is Uninitialized -> MainActivityViewState()
-                    }
-                )
+                _viewState.postValue(mapResult(it))
             }
         }
 
         disposables += rxInteractor.flowable
             .subscribeOn(Schedulers.io())
             .observeOn(Schedulers.io())
-            .subscribe({
-                _viewState.postValue(
-                    when (it) {
-                        is Success -> _viewState.value?.copy(posts = it.data, isLoading = false)
-                        is Loading -> viewState.value?.copy(isLoading = true)
-                        is Fail -> viewState.value?.copy(
-                            errorMessage = SingleEvent(Translation.error.unknownError),
-                            isLoading = false
-                        )
-                        is Uninitialized -> MainActivityViewState()
-                    }
-                )
-            }, {
+            .map(this::mapResult)
+            .subscribe(_viewState::postValue) {
+                Timber.e(it)
                 _viewState.postValue(
                     viewState.value?.copy(
                         errorMessage = SingleEvent(Translation.error.unknownError),
                         isLoading = false
                     )
                 )
-            })
+            }
     }
 
     fun fetchPosts() = scope.launch(Dispatchers.IO) { rxInteractor() }
 
     fun fetchPostsResult() = scope.launch {
         _viewState.value = MainActivityViewState(isLoading = true)
-        when (val result = withContext(Dispatchers.IO) { resultInteractor() }) {
-            is Success -> _viewState.value = _viewState.value?.copy(
-                isLoading = false,
-                posts = result.data
-            )
-            is Fail -> _viewState.value = _viewState.value?.copy(
-                isLoading = false,
-                errorMessage = SingleEvent(Translation.error.unknownError)
-            )
+        val result = withContext(Dispatchers.IO) { resultInteractor() }
+        _viewState.value = mapResult(result)
+    }
+
+    private fun mapResult(result: InteractorResult<List<Post>>): MainActivityViewState {
+        return when (result) {
+            is Success -> _viewState.value?.copy(posts = result.data, isLoading = false)
+                ?: MainActivityViewState(posts = result.data)
+            is Loading -> viewState.value?.copy(isLoading = true)
+                ?: MainActivityViewState(isLoading = true)
+            is Fail -> viewState.value?.copy(
+                errorMessage = SingleEvent(Translation.error.unknownError),
+                isLoading = false
+            ) ?: MainActivityViewState(errorMessage = SingleEvent(Translation.error.unknownError))
+            is Uninitialized -> MainActivityViewState()
         }
     }
 }
