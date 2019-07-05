4901b6ca74a59cc1411d12ebbc03c5f380275457
Alexander Bondarenco <sasha.199568@gmail.com>
2019-07-05 11:34:01 +0300

    Update nstack to 3.0.2

--- presentation/src/main/java/dk/nodes/template/presentation/ui/main/MainActivity+NStack.kt
+++ presentation/src/main/java/dk/nodes/template/presentation/ui/main/MainActivity+NStack.kt
@@ -6,13 +6,16 @@ import androidx.appcompat.app.AlertDialog
 import dk.nodes.nstack.kotlin.NStack
 import dk.nodes.nstack.kotlin.models.AppUpdate
 import dk.nodes.nstack.kotlin.models.AppUpdateState
+import dk.nodes.nstack.kotlin.models.Message
+import dk.nodes.nstack.kotlin.models.RateReminder
 import dk.nodes.template.presentation.nstack.Translation
 import timber.log.Timber
 
 fun MainActivity.setupNStack() {
     NStack.onAppUpdateListener = { appUpdate ->
         when (appUpdate.update.state) {
-            AppUpdateState.NONE -> {}
+            AppUpdateState.NONE -> {
+            }
             AppUpdateState.UPDATE -> {
                 showUpdateDialog(appUpdate.update)
             }
@@ -23,69 +26,90 @@ fun MainActivity.setupNStack() {
                 showChangelogDialog(appUpdate.update)
             }
         }
-        appUpdate.message?.let { message ->
-            showMessageDialog(message = message.message)
-        }
+        appUpdate.message?.let { showMessageDialog(it) }
+        appUpdate.rateReminder?.let { showRateReminderDialog(it) }
     }
     NStack.appOpen { success ->
         Timber.e("appopen success = $success")
     }
 }
 
-fun MainActivity.showMessageDialog(message: String) {
+fun MainActivity.showRateReminderDialog(rateReminder: RateReminder) {
     AlertDialog.Builder(this)
-            .setMessage(message)
-            .setPositiveButton(Translation.defaultSection.ok) { dialog, _ ->
-                dialog.dismiss()
-            }
-            .show()
+        .setMessage(rateReminder.body)
+        .setTitle(rateReminder.title)
+        .setCancelable(false)
+        .setPositiveButton(rateReminder.yesButton) { dialog, _ ->
+            NStack.onRateReminderAction(true)
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(rateReminder.link)))
+            dialog.dismiss()
+        }
+        .setNegativeButton(rateReminder.noButton) { dialog, _ ->
+            NStack.onRateReminderAction(false)
+            dialog.dismiss()
+        }
+        .setNeutralButton(rateReminder.laterButton) { dialog, _ ->
+            dialog.dismiss()
+        }
+        .show()
+}
+
+fun MainActivity.showMessageDialog(message: Message) {
+    AlertDialog.Builder(this)
+        .setMessage(message.message)
+        .setCancelable(false)
+        .setPositiveButton(Translation.defaultSection.ok) { dialog, _ ->
+            NStack.messageSeen(message)
+            dialog.dismiss()
+        }
+        .show()
 }
 
 fun MainActivity.showUpdateDialog(appUpdate: AppUpdate) {
     AlertDialog.Builder(this)
-            .setTitle(appUpdate.update?.translate?.title ?: return)
-            .setMessage(appUpdate.update?.translate?.message ?: return)
-            .setPositiveButton(appUpdate.update?.translate?.positiveButton) { dialog, _ ->
-                dialog.dismiss()
-            }
-            .show()
+        .setTitle(appUpdate.update?.translate?.title ?: return)
+        .setMessage(appUpdate.update?.translate?.message ?: return)
+        .setPositiveButton(appUpdate.update?.translate?.positiveButton) { dialog, _ ->
+            dialog.dismiss()
+        }
+        .show()
 }
 
 fun MainActivity.showChangelogDialog(appUpdate: AppUpdate) {
     AlertDialog.Builder(this)
-            .setTitle(appUpdate.update?.translate?.title ?: return)
-            .setMessage(appUpdate.update?.translate?.message ?: return)
-            .setNegativeButton(appUpdate.update?.translate?.negativeButton ?: return) { dialog, _ ->
-                dialog.dismiss()
-            }
-            .show()
+        .setTitle(appUpdate.update?.translate?.title ?: return)
+        .setMessage(appUpdate.update?.translate?.message ?: return)
+        .setNegativeButton(appUpdate.update?.translate?.negativeButton ?: return) { dialog, _ ->
+            dialog.dismiss()
+        }
+        .show()
 }
 
 fun MainActivity.startPlayStore() {
     try {
         startActivity(
-                Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse("market://details?id=$packageName")
-                )
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("market://details?id=$packageName")
+            )
         )
     } catch (anfe: android.content.ActivityNotFoundException) {
         startActivity(
-                Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
-                )
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
+            )
         )
     }
 }
 
 fun MainActivity.showForceDialog(appUpdate: AppUpdate) {
     val dialog = AlertDialog.Builder(this)
-            .setTitle(appUpdate.update?.translate?.title ?: return)
-            .setMessage(appUpdate.update?.translate?.message ?: return)
-            .setCancelable(false)
-            .setPositiveButton(appUpdate.update?.translate?.positiveButton, null)
-            .create()
+        .setTitle(appUpdate.update?.translate?.title ?: return)
+        .setMessage(appUpdate.update?.translate?.message ?: return)
+        .setCancelable(false)
+        .setPositiveButton(appUpdate.update?.translate?.positiveButton, null)
+        .create()
 
     dialog.setOnShowListener {
         val b = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
