22151b2ab373df07f7d969663809eadb913071ce
Roman <levinzon.roman@gmail.com>
2020-03-20 16:06:58 +0100

    Added basic OAuthCallback and OAuthRepository implementations

---
+++ data/src/main/java/dk/nodes/template/data/network/oauth/OAuthCallbackImpl.kt
@@ -0,0 +1,15 @@
+package dk.nodes.template.data.network.oauth
+
+import dk.nodes.okhttputils.oauth.OAuthCallback
+import dk.nodes.okhttputils.oauth.entities.OAuthInfo
+import dk.nodes.okhttputils.oauth.entities.OAuthResult
+import javax.inject.Inject
+
+class OAuthCallbackImpl @Inject constructor() : OAuthCallback {
+    override fun provideAuthInfo(refreshToken: String?): OAuthResult<OAuthInfo> {
+        return  OAuthResult.Success(OAuthInfo(
+                accessToken = "newAccessToken",
+                refreshToken = "new/old refresh token"
+        ))
+    }
+}
